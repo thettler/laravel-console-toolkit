6f499548956e6a38d961d038651c247e917df6d9
Tobias Hettler <tobias.hettler@bitbench.dev>
2023-05-07 18:31:57 +0200

    Update and fix PHPStan

--- src/Transfers/InputErrorData.php
+++ src/Transfers/InputErrorData.php
@@ -2,10 +2,17 @@
 
 namespace Thettler\LaravelConsoleToolkit\Transfers;
 
+use Thettler\LaravelConsoleToolkit\Contracts\ConsoleInput;
 use Thettler\LaravelConsoleToolkit\Reflections\InputReflection;
 
 class InputErrorData
 {
+    /**
+     * @param  string  $key
+     * @param  array  $choices
+     * @param  InputReflection<ConsoleInput>  $reflection
+     * @param  bool  $hasAutoAsk
+     */
     public function __construct(
         public readonly string $key,
         public readonly array $choices,
